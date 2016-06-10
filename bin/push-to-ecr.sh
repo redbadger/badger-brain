97bea8cc6ecad6272e955475b81e8114dac99625
Chris Goodchild <seegoodchild@gmail.com>
2016-06-10 16:17:36 +0100

    trying out an alternate eval for docker login and added latest tag label to repo url

--- bin/push-to-ecr.sh
+++ bin/push-to-ecr.sh
@@ -6,4 +6,4 @@ ECR=578418881509.dkr.ecr.us-east-1.amazonaws.com/badger-brain
 # Deploy image to ECR
 docker tag badger-brain:latest $ECR:$SHA1
 docker tag -f badger-brain:latest $ECR:latest
-docker push $ECR
+docker push $ECR:latest

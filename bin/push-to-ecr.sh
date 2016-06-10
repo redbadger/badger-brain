323bae7fbcd0ab0397b594765f4ab911138d1301
Chris Goodchild <seegoodchild@gmail.com>
2016-06-10 15:50:19 +0100

    few adjustments to ecr push script

--- bin/push-to-ecr.sh
+++ bin/push-to-ecr.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 SHA1=$1
-WEB=578418881509.dkr.ecr.us-east-1.amazonaws.com/badger-brain
+ECR=578418881509.dkr.ecr.us-east-1.amazonaws.com/badger-brain
 
 # Deploy image to ECR
-docker tag badger-brain $WEB:$SHA1
-docker tag -f badger-brain $WEB:latest
-docker push $WEB
+docker tag badger-brain:latest $ECR:$SHA1
+docker tag -f badger-brain:latest $ECR:latest
+docker push $ECR

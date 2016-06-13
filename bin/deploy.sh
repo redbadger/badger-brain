a046222bb93a2487a704a204a38199386691fc71
Chris Goodchild <seegoodchild@gmail.com>
2016-06-13 12:50:29 +0100

    added region to eb commands

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -32,8 +32,8 @@ aws s3 cp $ZIP s3://$EB_BUCKET/$ZIP
 
 # Create a new application version with the zipped up Dockerrun file
 aws elasticbeanstalk create-application-version --application-name $REPO_NAME-$TAG \
-    --version-label $TAG --source-bundle S3Bucket=$EB_BUCKET,S3Key=$ZIP
+    --version-label $TAG --source-bundle S3Bucket=$EB_BUCKET,S3Key=$ZIP --region eu-west-1
 
 # Update the environment to use the new application version
 aws elasticbeanstalk update-environment --environment-name $REPO_NAME-$TAG \
-      --version-label $TAG
+      --version-label $TAG --region eu-west-1

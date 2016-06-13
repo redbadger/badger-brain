1758cb22bc3ab79e84b048167f8a99d927107a9b
Chris Goodchild <seegoodchild@gmail.com>
2016-06-13 12:40:06 +0100

    initialised EB

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,24 +4,25 @@ TAG=$1
 SHA1=$2
 AWS_ACCOUNT=$3
 
+ZIP=$TAG.zip
 REPO_NAME=badger-brain
 REPO_URL=$AWS_ACCOUNT.dkr.ecr.eu-west-1.amazonaws.com/$REPO_NAME
 EB_BUCKET=elasticbeanstalk-eu-west-1-$AWS_ACCOUNT
 
 # Authenticate
-eval $(aws ecr get-login --region=us-east-1)
+eval $(aws ecr get-login --region=eu-west-1)
 
 # Build Docker image
 docker build -t $REPO_NAME .
 
 # Tag and deploy image to ECR
 docker tag $REPO_NAME:$TAG $REPO_URL:$SHA1
-docker tag -f $REPO_NAME:$TAG $REPO_URL:$TAG
+docker tag $REPO_NAME:$TAG $REPO_URL:$TAG
 docker push $REPO_URL
 
 # Apply correct repository and tag to Dockerrun.aws.json
-sed -i='' "s/<REPO_URL>/$REPO_URL/" Dockerrun.aws.json
-sed -i='' "s/<TAG>/$TAG/" Dockerrun.aws.json
+# sed -i '' "s/<REPO_URL>/$REPO_URL/" Dockerrun.aws.json
+# sed -i '' "s/<TAG>/$TAG/" Dockerrun.aws.json
 
 # Zip up the Dockerrun file
 zip -r $ZIP Dockerrun.aws.json

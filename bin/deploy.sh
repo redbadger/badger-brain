e58bb1bc52d72c84a8c3a06b0ca7efbefff22f64
Chris Goodchild <seegoodchild@gmail.com>
2016-06-13 13:03:43 +0100

    switched to using hard coded repo name for now

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -31,9 +31,9 @@ zip -r $ZIP Dockerrun.aws.json
 aws s3 cp $ZIP s3://$EB_BUCKET/$ZIP
 
 # Create a new application version with the zipped up Dockerrun file
-aws elasticbeanstalk create-application-version --application-name $REPO_NAME-$TAG \
+aws elasticbeanstalk create-application-version --application-name $REPO_NAME-staging \
     --version-label $TAG --source-bundle S3Bucket=$EB_BUCKET,S3Key=$ZIP --region eu-west-1
 
 # Update the environment to use the new application version
-aws elasticbeanstalk update-environment --environment-name $REPO_NAME-$TAG \
+aws elasticbeanstalk update-environment --environment-name $REPO_NAME-staging \
       --version-label $TAG --region eu-west-1

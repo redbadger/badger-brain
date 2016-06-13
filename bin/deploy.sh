b8e00725db182929a189db558d9aa6e0b9b8da2e
Chris Goodchild <seegoodchild@gmail.com>
2016-06-13 11:58:14 +0100

    first attempt deploying to EB using ECR image

---
+++ bin/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+TAG=$1
+SHA1=$2
+AWS_ACCOUNT=$3
+
+REPO_NAME=badger-brain
+REPO_URL=$AWS_ACCOUNT.dkr.ecr.eu-west-1.amazonaws.com/$REPO_NAME
+EB_BUCKET=elasticbeanstalk-eu-west-1-$AWS_ACCOUNT
+
+# Authenticate
+eval $(aws ecr get-login --region=us-east-1)
+
+# Build Docker image
+docker build -t $REPO_NAME .
+
+# Tag and deploy image to ECR
+docker tag $REPO_NAME:$TAG $REPO_URL:$SHA1
+docker tag -f $REPO_NAME:$TAG $REPO_URL:$TAG
+docker push $REPO_URL
+
+# Apply correct repository and tag to Dockerrun.aws.json
+sed -i='' "s/<REPO_URL>/$REPO_URL/" Dockerrun.aws.json
+sed -i='' "s/<TAG>/$TAG/" Dockerrun.aws.json
+
+# Zip up the Dockerrun file
+zip -r $ZIP Dockerrun.aws.json
+
+# Copy Zip file to S3 bucket
+aws s3 cp $ZIP s3://$EB_BUCKET/$ZIP
+
+# Create a new application version with the zipped up Dockerrun file
+aws elasticbeanstalk create-application-version --application-name $REPO_NAME-$TAG \
+    --version-label $TAG --source-bundle S3Bucket=$EB_BUCKET,S3Key=$ZIP
+
+# Update the environment to use the new application version
+aws elasticbeanstalk update-environment --environment-name $REPO_NAME-$TAG \
+      --version-label $TAG
--- bin/push-to-ecr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SHA1=$1
-ECR=578418881509.dkr.ecr.us-east-1.amazonaws.com/badger-brain
-
-# Deploy image to ECR
-docker tag badger-brain:latest $ECR:$SHA1
-docker tag -f badger-brain:latest $ECR:latest
-docker push $ECR

11cae9bc53a52fde4a54ec83a8aa0ceaa0a56e04
Chris Goodchild <seegoodchild@gmail.com>
2016-06-13 13:06:59 +0100

    switching names around and simplifying things

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-TAG=$1
-SHA1=$2
-AWS_ACCOUNT=$3
+SHA1=$1
+AWS_ACCOUNT=$2
 
-ZIP=$TAG.zip
+ZIP=staging.zip
 REPO_NAME=badger-brain
 REPO_URL=$AWS_ACCOUNT.dkr.ecr.eu-west-1.amazonaws.com/$REPO_NAME
 EB_BUCKET=elasticbeanstalk-eu-west-1-$AWS_ACCOUNT
@@ -16,8 +15,8 @@ eval $(aws ecr get-login --region=eu-west-1)
 docker build -t $REPO_NAME .
 
 # Tag and deploy image to ECR
-docker tag $REPO_NAME:$TAG $REPO_URL:$SHA1
-docker tag $REPO_NAME:$TAG $REPO_URL:$TAG
+docker tag $REPO_NAME:latest $REPO_URL:$SHA1
+docker tag $REPO_NAME:latest $REPO_URL:latest
 docker push $REPO_URL
 
 # Apply correct repository and tag to Dockerrun.aws.json
@@ -32,8 +31,8 @@ aws s3 cp $ZIP s3://$EB_BUCKET/$ZIP
 
 # Create a new application version with the zipped up Dockerrun file
 aws elasticbeanstalk create-application-version --application-name $REPO_NAME-staging \
-    --version-label $TAG --source-bundle S3Bucket=$EB_BUCKET,S3Key=$ZIP --region eu-west-1
+    --version-label staging --source-bundle S3Bucket=$EB_BUCKET,S3Key=$ZIP --region eu-west-1
 
 # Update the environment to use the new application version
 aws elasticbeanstalk update-environment --environment-name $REPO_NAME-staging \
-      --version-label $TAG --region eu-west-1
+      --version-label staging --region eu-west-1

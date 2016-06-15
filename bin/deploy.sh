36919fb7f310a7d0b63cecca12f3af1020a62a70
Chris Goodchild <seegoodchild@gmail.com>
2016-06-15 11:33:42 +0100

    added tag to build name - Docker just started having issues with this.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -21,7 +21,7 @@ fi
 eval $(aws ecr get-login --region=$AWS_REGION)
 
 # Build, tag and deploy Docker image to ECR
-docker build -t $APP_NAME .
+docker build -t $APP_NAME:$ENV .
 docker tag $APP_NAME:$ENV $ECR_REPO:$BUILD_SHA1
 docker tag -f $APP_NAME:$ENV $ECR_REPO:$ENV
 docker push $ECR_REPO

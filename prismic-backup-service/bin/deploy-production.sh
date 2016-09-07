c7a663d8fe033ead9c40a223edddc5e37349eb1d
Zoe Bryant <zoeabryant@gmail.com>
2016-09-07 16:19:42 +0100

    Production deploy script for prismic backup service

---
+++ prismic-backup-service/bin/deploy-production.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SERVICE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+ENV_FILE="$SERVICE_DIR/config/env.json"
+
+echo Generating env.json
+
+cat > "$ENV_FILE" << Heredoc
+{
+  "bucketName": "redbadger-prismic-backup-prod",
+  "ErrorSNSSubscriptions": [
+    {
+      "Endpoint" : "$BADGER_LABS_SLACK_EMAIL",
+      "Protocol" : "email"
+    },
+    {
+      "Endpoint" : "$BADGER_LABS_EMAIL",
+      "Protocol" : "email"
+    }
+  ]
+}
+Heredoc
+
+echo Generated env.json
+
+echo Deploying prismic-backup-service to prod
+
+cd "$SERVICE_DIR"
+sls deploy --stage prod
+
+echo Cleanup
+rm "$ENV_FILE"
--- prismic-backup-service/bin/handler.js
+++ prismic-backup-service/bin/handler.js
@@ -1,6 +1,6 @@
 'use strict'; // eslint-disable-line strict
 const backup = require('../lib');
-const env = require('../config/env.json');
+const env = require('../config/env.json'); // eslint-disable-line import/no-unresolved
 
 module.exports.backupPrismic = (event, context, cb) => {
   backup(env.bucketName)

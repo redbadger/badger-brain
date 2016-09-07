959be33ea5d186199fa92fd696429c56bca90af8
Zoe Bryant <zoeabryant@gmail.com>
2016-09-07 13:57:12 +0100

    Adding notifications if the service fails

--- prismic-backup-service/bin/handler.js
+++ prismic-backup-service/bin/handler.js
@@ -4,18 +4,22 @@ const variables = require('../config/variables.json');
 
 const environment = (cb, functionName) => {
   switch (functionName) {
-    case 'prismic-backup-service-dev-backupPrismic':
+    case 'prismic-backup-service-dev-BackupPrismic':
       return variables.dev;
-    case 'prismic-backup-service-prod-backupPrismic':
+    case 'prismic-backup-service-prod-BackupPrismic':
       return variables.prod;
     default:
-      return cb('Unable to determine environment.');
+      return null;
   }
 };
 
 module.exports.backupPrismic = (event, context, cb) => {
   const env = environment(cb, context.functionName);
-  backup(env.bucketName)
-    .then(metadata => cb(null, metadata))
-    .catch(err => cb(err));
+  if (!env) {
+    cb(new Error('Unable to determine environment.'));
+  } else {
+    backup(env.bucketName)
+      .then(metadata => cb(null, metadata))
+      .catch(err => cb(err));
+  }
 };

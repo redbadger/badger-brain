1ae857b8e52428cacd319e76e60bbc4c46a478e1
Zoe Bryant <zoeabryant@gmail.com>
2016-09-07 14:35:43 +0100

    Alert Emails in a config files

--- prismic-backup-service/bin/handler.js
+++ prismic-backup-service/bin/handler.js
@@ -1,25 +1,9 @@
 'use strict'; // eslint-disable-line strict
 const backup = require('../lib');
-const variables = require('../config/variables.json');
-
-const environment = (cb, functionName) => {
-  switch (functionName) {
-    case 'prismic-backup-service-dev-BackupPrismic':
-      return variables.dev;
-    case 'prismic-backup-service-prod-BackupPrismic':
-      return variables.prod;
-    default:
-      return null;
-  }
-};
+const env = require('../config/env.json');
 
 module.exports.backupPrismic = (event, context, cb) => {
-  const env = environment(cb, context.functionName);
-  if (!env) {
-    cb(new Error('Unable to determine environment.'));
-  } else {
-    backup(env.bucketName)
-      .then(metadata => cb(null, metadata))
-      .catch(err => cb(err));
-  }
+  backup(env.bucketName)
+    .then(metadata => cb(null, metadata))
+    .catch(err => cb(err));
 };
